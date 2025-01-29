2823a4eb1e414944a381a0e118130284c45b1f16
oweeeenie <owensullivan419@yahoo.com>
2025-01-28 20:45:36 -0500

    added prettier, eslint fix, header and rendered Components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import './App.css'
+import "./App.css";
+import Components from "./Components";
 
 function App() {
   return (
-     <>
-     <h1 className='text-red-500'>idk</h1>
-     <h2>idk</h2>
-     </>
-  )
+    <>
+      <Components />
+    </>
+  );
 }
 
-
-export default App
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,21 @@
+function Header() {
+  return (
+    <>
+      <div className='header-container flex justify-between p-4 text-[var(--main-text)]'>
+        <div className='left-header'>
+          <h1 className='game-name text-3xl'>Pokemon Memory Game</h1>
+          <p className='game-rules text-xl py-4'>
+            Get points by clicking an image but don&apos;t click on any image
+            more than once!
+          </p>
+        </div>
+        <div className='right-header'>
+          <p className='best-score'>Best Score: 4</p>
+          <p className='score'>Score: 1</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
