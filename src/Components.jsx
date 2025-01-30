0b173fba90ee7832e1e0960fbc5f0e693342f28e
oweeeenie <owensullivan419@yahoo.com>
2025-01-29 19:52:39 -0500

    generated cards + styled

--- src/Components.jsx
+++ src/Components.jsx
@@ -1,9 +1,11 @@
 import Header from "./Header";
+import Memorycard from "./Memorycard";
 
 export default function Components() {
   return (
     <>
       <Header />
+      <Memorycard />
     </>
   );
 }
